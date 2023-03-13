43bcdbced7557dc2e1ca3a6db2c327f139f29675
Viral Sangani <viral.sangani2011@gmail.com>
2023-03-13 19:19:44 +0530

    Feat: header updated 🔥

--- packages/frontend/components/Header.tsx
+++ packages/frontend/components/Header.tsx
@@ -1,35 +1,62 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { CustomConnectButton } from "./CustomConnectButton";
 
 export default function Header() {
+  const router = useRouter();
+  const getActiveTab = () => {
+    switch (router.pathname) {
+      case "/collection":
+        return "collection";
+      case "/mint":
+        return "mint";
+      case "/create":
+        return "create";
+      default:
+        return "index";
+    }
+  };
   return (
     <>
       <div className="mx-auto md:max-w-7xl w-full md:px-2 px-5 sm:px-6 lg:px-8">
         <div className="hidden relative md:flex h-16 justify-between">
-          <div className=" md:flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="flex items-center justify-center">
             <div className="text-4xl text-center md:flex items-center">🍞</div>
-            <div className="sm:ml-6 sm:flex sm:space-x-8">
-              <a
-                href="#"
-                className="inline-flex items-center font-bold text-base text-gray-900"
+            <div className="sm:ml-6">
+              <Link
+                href="/collection"
+                className={`font-bold text-base text-gray-900 px-4 py-2 rounded-xl ${
+                  getActiveTab() === "collection"
+                    ? "bg-white border border-black"
+                    : ""
+                }`}
               >
                 🫙 Collection
-              </a>
+              </Link>
             </div>
             <div className="sm:ml-6 sm:flex sm:space-x-8">
-              <a
-                href="#"
-                className="inline-flex items-center font-bold text-base text-gray-900"
+              <Link
+                href="/mint"
+                className={`font-bold text-base text-gray-900 px-4 py-1 rounded-xl ${
+                  getActiveTab() === "mint"
+                    ? "bg-white border border-black"
+                    : ""
+                }`}
               >
                 🫡 Mint
-              </a>
+              </Link>
             </div>
             <div className="sm:ml-6 sm:flex sm:space-x-8">
-              <a
-                href="#"
-                className="inline-flex items-center font-bold text-base text-gray-900"
+              <Link
+                href="/create"
+                className={`font-bold text-base text-gray-900 px-4 py-1 rounded-xl ${
+                  getActiveTab() === "create"
+                    ? "bg-white border border-black"
+                    : ""
+                }`}
               >
                 🍻 Create
-              </a>
+              </Link>
             </div>
           </div>
           <div className="md:absolute md:inset-y-0 md:right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -45,28 +72,38 @@ export default function Header() {
       <div className="md:hidden fixed bottom-0 left-0 z-50 w-full h-14 bg-background border-t-2 border-black">
         <div className="h-full max-w-lg flex flex-row flex-nowrap justify-evenly items-center mx-auto">
           <div className="sm:ml-6 sm:flex sm:space-x-8">
-            <a
-              href="#"
-              className="inline-flex items-center font-bold text-base text-gray-900"
+            <Link
+              href="/collection"
+              className={`font-bold text-base text-gray-900 px-2 py-1 rounded-xl ${
+                getActiveTab() === "collection"
+                  ? "bg-white border border-black"
+                  : ""
+              }`}
             >
               🫙 Collection
-            </a>
+            </Link>
           </div>
           <div className="sm:ml-6 sm:flex sm:space-x-8">
-            <a
-              href="#"
-              className="inline-flex items-center font-bold text-base text-gray-900"
+            <Link
+              href="/mint"
+              className={`font-bold text-base text-gray-900 px-2 py-1 rounded-xl ${
+                getActiveTab() === "mint" ? "bg-white border border-black" : ""
+              }`}
             >
               🫡 Mint
-            </a>
+            </Link>
           </div>
           <div className="sm:ml-6 sm:flex sm:space-x-8">
-            <a
-              href="#"
-              className="inline-flex items-center font-bold text-base text-gray-900"
+            <Link
+              href="/create"
+              className={`font-bold text-base text-gray-900 px-2 py-1 rounded-xl ${
+                getActiveTab() === "create"
+                  ? "bg-white border border-black"
+                  : ""
+              }`}
             >
               🍻 Create
-            </a>
+            </Link>
           </div>
         </div>
       </div>
---
+++ packages/frontend/pages/mint.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center md:pt-20 pt-4 w-full">
+        Mint
+      </div>
+    </>
+  );
+}
