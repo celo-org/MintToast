f63980896aa505cf42557682990c2a323d63d84e
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-03-23 13:05:25 +0530

    feat: subgraph

---
+++ packages/subgraph/src/badge.ts
@@ -0,0 +1,108 @@
+import { Address, BigInt, log } from "@graphprotocol/graph-ts";
+import {
+    Badge,
+    ApprovalForAll,
+    CollectionCreated,
+    Initialized,
+    RoleAdminChanged,
+    RoleGranted,
+    RoleRevoked,
+    TransferBatch,
+    TransferSingle,
+    URI,
+} from "../generated/Badge/Badge";
+import { Serie, User, Item, SeriesBalance } from "../generated/schema";
+
+export function handleCollectionCreated(event: CollectionCreated): void {
+    let collectionId = event.params.collectionId;
+    let series = new Serie(collectionId.toString());
+    series.currentSupply = new BigInt(0);
+    series.creationTimestamp = event.block.timestamp;
+    series.totalSupply = event.params.supplyCollection;
+    series.toaster = event.params.creator;
+
+    let contract = Badge.bind(event.address);
+    series.uri = contract.uri(collectionId);
+
+    series.save();
+}
+
+export function handleInitialized(event: Initialized): void {}
+
+export function handleRoleAdminChanged(event: RoleAdminChanged): void {}
+
+export function handleRoleGranted(event: RoleGranted): void {}
+
+export function handleRoleRevoked(event: RoleRevoked): void {}
+
+export function handleTransferBatch(event: TransferBatch): void {}
+
+export function handleTransferSingle(event: TransferSingle): void {
+    // if (
+    //     event.params.from.toHexString() ==
+    //     "0x0000000000000000000000000000000000000000"
+    // ) {
+    // log.debug("{}, {}", [
+    //     event.params.from.toHexString(),
+    //     event.params.to.toHexString(),
+    // ]);
+
+    let collection = Serie.load(event.params.id.toString());
+    if (collection) {
+        let contract = Badge.bind(event.address);
+        collection.currentSupply = contract
+            .currentSupply(event.params.id)
+            .minus(new BigInt(1));
+        collection.save();
+
+        let item = new Item(
+            contract
+                .currentSupply(event.params.id)
+                .minus(new BigInt(1))
+                .toString()
+        );
+        item.serie = collection.id;
+        item.owner = event.params.to.toHexString();
+        item.timestamp = event.block.timestamp;
+        item.save();
+        let user = User.load(event.params.to.toString());
+        if (user) {
+            user.collection.push(item.id);
+            user.save();
+        } else {
+            let user = new User(event.params.to.toHexString());
+            user.save();
+        }
+        // }
+    }
+    // else if (event.params.to == new Address(0)) {
+    //     //TODO
+    // } else {
+    //     let sender = User.load(event.params.from.toString());
+    //     if (sender) {
+    //         let itemIndex = sender.collection.indexOf(
+    //             event.params.id.toString()
+    //         );
+    //         sender.collection[itemIndex] =
+    //             sender.collection[sender.collection.length];
+    //         sender.collection.pop();
+    //         sender.save();
+    //     }
+    //     let receiver = User.load(event.params.to.toString());
+    //     if (receiver) {
+    //         receiver.collection.push(event.params.id.toString());
+    //         receiver.save();
+    //     } else {
+    //         let user = new User(event.params.to.toString());
+    //         user.collection.push(event.params.id.toString());
+    //         user.save();
+    //     }
+    // }
+}
+
+export function handleURI(event: URI): void {
+    let series = Serie.load(event.params.id.toString());
+    if (series) {
+        series.uri = `ipfs://${event.params.value}`;
+    }
+}
