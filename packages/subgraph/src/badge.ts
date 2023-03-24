80d38842c080aa47d08002b120fb0c25f90840cf
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-03-24 11:22:47 +0530

    fix: Serie -> Event

--- packages/subgraph/src/badge.ts
+++ packages/subgraph/src/badge.ts
@@ -2,72 +2,62 @@ import { BigInt } from "@graphprotocol/graph-ts";
 import {
     Badge,
     CollectionCreated,
-    Initialized,
-    RoleAdminChanged,
-    RoleGranted,
-    RoleRevoked,
-    TransferBatch,
     TransferSingle,
     URI,
 } from "../generated/Badge/Badge";
-import { Serie, User, Item } from "../generated/schema";
+import { Event, User, Item } from "../generated/schema";
 
 export function handleCollectionCreated(event: CollectionCreated): void {
-    /**
-     * collectionId - Id of the serie.
-     */
     let collectionId = event.params.collectionId;
+    let events = new Event(collectionId.toString());
+    events.currentSupply = new BigInt(0);
+    events.creationTimestamp = event.block.timestamp;
+    events.totalSupply = event.params.supplyCollection;
+    events.toaster = event.params.creator;
 
-    let series = new Serie(collectionId.toString());
-    series.currentSupply = new BigInt(0);
-    series.creationTimestamp = event.block.timestamp;
-    series.totalSupply = event.params.supplyCollection;
-    series.toaster = event.params.creator;
-
-    /**
-     * Fetching the uri for the serie from the contract
-     */
     let contract = Badge.bind(event.address);
-    series.uri = contract.uri(collectionId);
+    events.uri = contract.uri(collectionId);
 
-    series.save();
+    events.save();
 }
 
 export function handleTransferSingle(event: TransferSingle): void {
-    /**
-     * If `from` is zero address then it is a mint.
-     */
-    if (
-        event.params.from.toHexString() ==
-        "0x0000000000000000000000000000000000000000"
-    ) {
-        let collection = Serie.load(event.params.id.toString());
-        if (collection) {
-            let contract = Badge.bind(event.address);
-            collection.currentSupply = contract
-                .currentSupply(event.params.id)
-                .minus(new BigInt(1));
-            collection.save();
+    // if (
+    //     event.params.from.toHexString() ==
+    //     "0x0000000000000000000000000000000000000000"
+    // ) {
+    // log.debug("{}, {}", [
+    //     event.params.from.toHexString(),
+    //     event.params.to.toHexString(),
+    // ]);
 
-            let item = new Item(
-                contract
-                    .currentSupply(event.params.id)
-                    .minus(new BigInt(1))
-                    .toString()
-            );
-            item.serie = collection.id;
-            item.owner = event.params.to.toHexString();
-            item.timestamp = event.block.timestamp;
-            item.save();
-            let user = User.load(event.params.to.toString());
-            if (user) {
-                user.collection.push(item.id);
-                user.save();
-            } else {
-                let user = new User(event.params.to.toHexString());
-                user.save();
-            }
+    let collection = Event.load(event.params.id.toString());
+    if (collection) {
+        let contract = Badge.bind(event.address);
+        collection.currentSupply = contract
+            .currentSupply(event.params.id)
+            .minus(new BigInt(1));
+        collection.save();
+
+        let item = new Item(
+            contract
+                .currentSupply(event.params.id)
+                .minus(new BigInt(1))
+                .toString()
+        );
+        item.event = collection.id;
+        item.owner = event.params.to.toHexString();
+        item.timestamp = event.block.timestamp;
+        item.save();
+        let user = User.load(event.params.to.toString());
+        if (user) {
+            user.collection.push(item.id);
+            user.save();
+        } else {
+            let user = new User(event.params.to.toHexString());
+            user.save();
         }
+        // }
     }
     // else if (event.params.to == new Address(0)) {
     //     //TODO
@@ -95,10 +85,7 @@ export function handleTransferSingle(event: TransferSingle): void {
 }
 
 export function handleURI(event: URI): void {
-    /**
-     * If the uri of the serie is changed then change it in the graph
-     */
-    let series = Serie.load(event.params.id.toString());
+    let series = Event.load(event.params.id.toString());
     if (series) {
         series.uri = `ipfs://${event.params.value}`;
     }
