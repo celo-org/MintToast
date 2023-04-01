5f3e12b613709f434e2ebb9f60970069864950ca
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-04-01 14:29:42 +0530

    feat: add Tests

--- packages/contract/test/Badge.js
+++ packages/contract/test/Badge.js
@@ -1,44 +1,99 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { utils, constants } = ethers;
+
+const BASE_URI = "ipfs://";
+
+async function deployBadge(defaultAdmin) {
+    const Badge = await ethers.getContractFactory("Badge");
+    let badge = await upgrades.deployProxy(Badge, [
+        BASE_URI,
+        defaultAdmin.address,
+    ]);
+    await badge.deployed();
+
+    let proxyAdmin = await upgrades.erc1967.getAdminAddress(badge.address);
+    let badgeImplementation = await upgrades.erc1967.getImplementationAddress(
+        badge.address
+    );
+
+    return { badge, badgeImplementation, proxyAdmin };
+}
 
 describe("Badge Test", () => {
-    let deployer, user1, user2;
-    async function deployBadge() {
-        [deployer, user1, user2] = await ethers.getSigners();
-        const Badge = await ethers.getContractFactory("Badge");
-        let badge = await upgrades.deployProxy(Badge, [
-            "ipfs://",
-            deployer.address,
-        ]);
-        await badge.deployed();
+    let defaultAdmin, toastmaster, user1, user2;
+    let TOASTMASTER_ROLE = utils.keccak256(
+        utils.toUtf8Bytes("TOASTMASTER_ROLE")
+    );
 
-        return { badge };
-    }
-
-    it("Should be able to create collection", async () => {
-        let { badge } = await loadFixture(deployBadge);
-
-        await badge.grantRole(await badge.TOASTMASTER_ROLE(), deployer.address);
-
-        await badge.createCollection(1, "23849u2384bjb");
-        expect(await badge.collections(), 1);
-        await badge.mint(deployer.address, 0);
-        expect(await badge.balanceOf(deployer.address, 0), 1);
+    beforeEach(async () => {
+        [defaultAdmin, toastmaster, user1, user2] = await ethers.getSigners();
     });
 
-    it("Should be able to mint", async () => {
-        let { badge } = await loadFixture(deployBadge);
+    describe("Initialization", () => {
+        let badge;
 
-        await badge.grantRole(await badge.TOASTMASTER_ROLE(), deployer.address);
+        beforeEach(async () => {
+            ({ badge } = await deployBadge(defaultAdmin));
+        });
 
-        await badge.createCollection(2, "23849u2384bjb");
-        expect(await badge.collections()).to.be.eq(1);
+        it('Base URI should be equal to ""', async () => {
+            expect(await badge.uri(0)).to.be.eq("");
+        });
 
-        await badge.mint(user1.address, 0);
-        await badge.mint(deployer.address, 0);
+        it("Deployer should have DEFAULT_ADMIN_ROLE", async () => {
+            expect(
+                await badge.hasRole(constants.HashZero, defaultAdmin.address)
+            ).to.be.true;
+        });
 
-        expect(await badge.balanceOf(user1.address, 0)).to.be.eq(1);
-        expect(await badge.balanceOf(deployer.address, 0)).to.be.eq(1);
+        it("Protocol should be in UNPAUSED state", async () => {
+            expect(await badge.paused()).to.be.false;
+        });
+
+        it("TOASTMASTER_ROLE should be keccak of TOASTMASTER_ROLE", async () => {
+            expect(
+                await badge.connect(defaultAdmin).TOASTMASTER_ROLE()
+            ).to.be.eq(TOASTMASTER_ROLE);
+        });
+
+        it("DEFAULT_ADMIN_ROLE should be bytes 0", async () => {
+            expect(
+                await badge.connect(defaultAdmin).DEFAULT_ADMIN_ROLE()
+            ).to.be.eq(constants.HashZero);
+        });
     });
+
+    describe("Admin Interaction", () => {
+        let badge, badgeImplementation, proxyAdmin;
+
+        before(async () => {
+            ({ badge, badgeImplementation, proxyAdmin } = await deployBadge(
+                defaultAdmin
+            ));
+        });
+
+        it("DEFAULT_ADMIN should be able to assign TOASTMASTER_ROLE", async () => {
+            await badge
+                .connect(defaultAdmin)
+                .grantRole(TOASTMASTER_ROLE, toastmaster.address);
+            expect(await badge.hasRole(TOASTMASTER_ROLE, toastmaster.address))
+                .to.be.true;
+        });
+
+        it("DEFAULT_ADMIN_ROLE should be able to pause protocol", async () => {
+            await badge.pause();
+            expect(await badge.paused()).to.be.true;
+        });
+        it("DEFAULT_ADMIN_ROLE should be able to unpause protocol", async () => {
+            await badge.unpause();
+            expect(await badge.paused()).to.be.false;
+        });
+    });
+
+    describe("Toast Masters Interaction", () => {});
+
+    describe("User Interactions", () => {});
+
+    describe("Performing Upgrades", () => {});
 });
