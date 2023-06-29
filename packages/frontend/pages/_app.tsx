174c6d998f74ade43ab8c863706afbeacffe326a
Viral Sangani <viral.sangani2011@gmail.com>
2023-06-30 00:28:38 +0530

    Feat: Support for WalletConnect V2 added

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,28 +1,20 @@
-import {
-  connectorsForWallets,
-  RainbowKitProvider,
-} from "@rainbow-me/rainbowkit";
+import celoGroups from "@celo/rainbowkit-celo/lists";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
-import {
-  metaMaskWallet,
-  omniWallet,
-  walletConnectWallet,
-} from "@rainbow-me/rainbowkit/wallets";
 import type { AppProps } from "next/app";
 import { Roboto_Mono } from "next/font/google";
 import NextNProgress from "nextjs-progressbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import "../styles/custom.css";
 import "../styles/globals.css";
 
 // Import known recommended wallets
-import { CeloDance, CeloWallet, Valora } from "@celo/rainbowkit-celo/wallets";
 
 // Import CELO chain information
-import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
+import { Alfajores, Cannoli, Celo } from "@celo/rainbowkit-celo/chains";
 
 import { GlobalContextProvider } from "@/context/GlobalContext";
 import { CAPTCH_SITEKEY } from "@/data/constant";
@@ -31,8 +23,10 @@ import { useRouter } from "next/router";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import Layout from "../components/Layout";
 
-const { chains, provider } = configureChains(
-  [Alfajores, Celo],
+const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string;
+console.log("projectId", projectId);
+const { chains, publicClient } = configureChains(
+  [Alfajores, Celo, Cannoli],
   [
     jsonRpcProvider({
       rpc: (chain) => ({ http: chain.rpcUrls.default.http[0] }),
@@ -40,24 +34,16 @@ const { chains, provider } = configureChains(
   ]
 );
 
-const connectors = connectorsForWallets([
-  {
-    groupName: "Recommended with CELO",
-    wallets: [
-      Valora({ chains }),
-      CeloWallet({ chains }),
-      CeloDance({ chains }),
-      metaMaskWallet({ chains }),
-      omniWallet({ chains }),
-      walletConnectWallet({ chains }),
-    ],
-  },
-]);
+const connectors = celoGroups({
+  chains,
+  projectId,
+  appName: "Mint Toast",
+});
 
-const wagmiClient = createClient({
+const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
-  provider,
+  publicClient: publicClient,
 });
 
 const roboto_mono = Roboto_Mono({
@@ -84,12 +70,12 @@ function App({ Component, pageProps }: AppProps) {
             href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🍞</text></svg>"
           />
           <meta
-            http-equiv="Content-Security-Policy"
+            httpEquiv="Content-Security-Policy"
             content="upgrade-insecure-requests"
           />
         </Head>
 
-        <WagmiConfig client={wagmiClient}>
+        <WagmiConfig config={wagmiConfig}>
           <RainbowKitProvider chains={chains} coolMode={true}>
             <GlobalContextProvider>
               <Layout>
